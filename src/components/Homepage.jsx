47d7b0bf8a897e3dedd929c4e4daa0f95324e8f0
Evin-Ngoa <toneevin07@gmail.com>
2021-11-28 03:44:31 +0300

    Minor Bug Fix in Home

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import millify from 'millify';
-import { Typography, Row, Col, Statistic, Button } from 'antd';
+import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import Spinner from './Spinner';
