b131fe75bee595c331b55198906c6aedfe39852d
Evin-Ngoa <toneevin07@gmail.com>
2021-11-28 22:48:42 +0300

    Resposive Menu design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, SlidersOutlined, ProfileOutlined , FundOutlined } from '@ant-design/icons';
+import { HomeOutlined, SlidersOutlined, ProfileOutlined , FundOutlined, MenuOutlined } from '@ant-design/icons';
 import icon from '../images/cryptocurrency.png';
 
 const Navbar = () => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(undefined);
+  
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+    
+        window.addEventListener('resize', handleResize);
+    
+        handleResize();
+    
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    useEffect(() => {
+        // mobile or tab size
+        if (screenSize <= 800) {
+            setActiveMenu(false);
+        } else {
+            setActiveMenu(true);
+        }
+    }, [screenSize]);
+
     return (
         <div className="nav-container">
             <div className="logo-container">
@@ -12,10 +34,12 @@ const Navbar = () => {
                 <Typography.Title level={2} className="logo">
                     <Link to="/">Evin Crypto</Link>
                 </Typography.Title>
-                <Button className="menu-control-container">
-
+                <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
+                    <MenuOutlined />
                 </Button>
             </div>
+            
+            {activeMenu && (
                 <Menu theme="dark">
                     <Menu.Item icon={<HomeOutlined />}>
                         <Link to="/">Home</Link>
@@ -30,6 +54,8 @@ const Navbar = () => {
                         <Link to="/news">News</Link>
                     </Menu.Item>
                 </Menu>
+            )}
+
             
         </div>
     )
